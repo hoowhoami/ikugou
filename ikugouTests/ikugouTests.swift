fa9ff05248729dafb2f1a749b3f62a02056746c6
jiangliangtong <jiangliangtong@gmail.com>
2025-04-27 16:09:08 +0800

    Initial Commit

---
+++ ikugouTests/ikugouTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikugouTests.swift
+//  ikugouTests
+//
+//  Created by 蒋梁通 on 2025/4/27.
+//
+
+import Testing
+@testable import ikugou
+
+struct ikugouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
